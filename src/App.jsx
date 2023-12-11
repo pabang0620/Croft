dcd31b6dd89903b4369dabc59957a9daec5aa2f9
Wonho <pabang0620@naver.com>
2023-12-11 20:35:25 +0900

    dev set

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className=" bg-green-100 font-extralight">확인용 테일윈드
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className=" bg-green-100">daf
+    </div>
+  );
+}
+
+export default NavBar;
