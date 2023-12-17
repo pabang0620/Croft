57d7cf791d6a04995ca08e7a3dcb7b8c47b92dd1
blue7918 <kkiu978@gmail.com>
2023-12-17 18:33:15 +0900

    feat: croft 가이드 추가 및 차트와 드래그 앤 드랍 테스트 중

--- src/pages/SingleDashBoard.js
+++ src/pages/SingleDashBoard.js
@@ -1,16 +1,15 @@
-import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import GreenhouseScore from '../component/Chart/GreenhouseScore';
+import CroftGuide from '../component/Chart/CroftGuide/CroftGuide';
 
 const SingleDashBoard = () => {
   const navigate = useNavigate();
-
+  //드래그 앤 드랍 추가 예정
   return (
     <div>
-      <div className="w-20 h-20 bg-white" onClick={() => navigate('/')}>
-        temp button
-      </div>
-      <div className="p-4">
-  
+      <div className="p-4 flex flex-row flex-wrap gap-[10px]">
+        <GreenhouseScore />
+        <CroftGuide />
       </div>
     </div>
   );
