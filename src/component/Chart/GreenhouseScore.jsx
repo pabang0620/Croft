e02925212fdb6a9c88e32e42edba8f474267f818
blue7918 <kkiu978@gmail.com>
2023-12-29 05:21:08 +0900

    feat: greenhouseScore part complete

--- src/component/Chart/GreenhouseScore.jsx
+++ src/component/Chart/GreenhouseScore.jsx
@@ -1,22 +1,17 @@
+import { ScoreText, ScoreImg } from '../utils/Data/GreenhouseScoreData';
+
 // 종합 온실관리 평가 스코어 Component
 //온실환경 종합 스코어 표시. 총 5개의 단계로 표시되며, 해당 단계를 표시할 수 있는 이미지 형태와, 가이드 텍스트 표시.
-
 const GreenhouseScore = () => {
+  const score = 5; //임의로 점수 설정
   return (
     <div className="flex flex-col w-[320px] h-[220px] p-[21px] gap-[17px] rounded-[10px] bg-white relative cursor-pointer">
-      <div className="flex justify-center">
-        <img
-          src="/assets/images/DashBoard/GreenhouseScoreGood.svg"
-          alt="plant"
-        ></img>
-      </div>
+      <div className="flex justify-center">{ScoreImg(score)}</div>
       <div className="flex flex-col gap-[2px] font-sans">
         <div className=" font-bold text-sm text-center">
-          온실 관리가 매우 잘 되고 있어요.
+          {ScoreText(score).title}
         </div>
-        <div className="text-xs text-center">
-          관리 노하우를 저한테도 알려주세요.
-        </div>
+        <div className="text-xs text-center">{ScoreText(score).content}</div>
       </div>
     </div>
   );
