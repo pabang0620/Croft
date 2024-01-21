10a7efb2aa0bcb3bc8e166f73b790448bd24adac
Wonho <pabang0620@naver.com>
2024-01-21 23:19:19 +0900

    레이아웃 수정

--- src/component/utils/DND/GridData.jsx
+++ src/component/utils/DND/GridData.jsx
@@ -1,108 +1,280 @@
-// WonhoGridData.jsx
 import React from "react";
 import MainSliderDiv from "../../Graphs/MainSliderDiv";
-
 import MainBarChartLine from "../../Charts/BarCharts/MainBarChartLine";
 import MainBarChart from "../../Charts/BarCharts/MainBarChart";
-
 import MainRTRLineChart from "../../Charts/LineCharts/MainRTRLineChart";
 import MainLineChart from "../../Charts/LineCharts/MainLineChart";
 import MainFootLineChart from "../../Charts/LineCharts/MainFootLineChart";
 import MainSmoothedLineChart from "../../Charts/LineCharts/MainSmoothedLineChart";
 import MainSmoothedLineChartAdd from "../../Charts/LineCharts/MainSmoothedLineChartAdd";
-
 import MainLineAreaChart from "../../Charts/MixCharts/MainLineAreaChart";
 import MainBarLine2Chart from "../../Charts/MixCharts/MainBarLine2Chart";
-
 import TotalReportChart from "../../Charts/TotalReportChart/TotalReportChart";
 import GreenhouseScore from "../../Charts/GreenhouseScore";
 import CroftGuide from "../../Charts/CroftGuide/CroftGuide";
 import TotalResourceChart from "../../Charts/TotalResourceChart/TotalResourceChart";
+const positionMap = {
+  0: { x: 0, y: 0, w: 4, h: 2 }, // width 4, height 2
+  1: { x: 4, y: 0, w: 2, h: 2 }, // width 2, height 2
+  2: { x: 6, y: 0, w: 4, h: 2 }, // width 4, height 2
+  3: { x: 0, y: 5, w: 4, h: 2 }, // width 4, height 2
+  4: { x: 0, y: 1, w: 2, h: 2 }, // width 2, height 2
+  5: { x: 2, y: 1, w: 2, h: 2 }, // width 2, height 2
+  6: { x: 4, y: 1, w: 3, h: 4 }, // width 3, height 4
+  7: { x: 8, y: 1, w: 3, h: 4 }, // width 3, height 4
+  8: { x: 0, y: 2, w: 2, h: 2 }, // width 2, height 2
+  9: { x: 2, y: 2, w: 2, h: 2 }, // width 2, height 2
+  10: { x: 0, y: 3, w: 2, h: 2 }, // width 2, height 2
+  11: { x: 2, y: 3, w: 2, h: 2 }, // width 2, height 2
+  12: { x: 4, y: 3, w: 2, h: 2 }, // width 2, height 2
+  13: { x: 0, y: 4, w: 2, h: 2 }, // width 2, height 2
+  14: { x: 2, y: 4, w: 2, h: 2 }, // width 2, height 2
+  15: { x: 4, y: 4, w: 2, h: 2 }, // width 2, height 2
+  16: { x: 6, y: 4, w: 2, h: 2 }, // width 2, height 2
+  17: { x: 0, y: 6, w: 2, h: 2 }, // width 2, height 2
+};
 
 const GridData = [
-  <TotalReportChart key="0" title="온실 환경 종합" time="10:25" size={50} />,
-  <GreenhouseScore key="1" />,
-  <CroftGuide key="2" />,
-  <TotalResourceChart key="3" />, //자원사용량 n월 n주차
-
-  <MainSliderDiv
-    key="4"
-    queryName="rtr"
-    title="RTR"
-    absData1="0"
-    absData2="1.2"
-    absData3="1.5"
-    absData4="3"
-    absData5="영양"
-    absData6="균형"
-    absData7="생식"
-    absData8="생식생장, 꽃이 피고 열매가 맺혀요"
-  />,
-  <MainSliderDiv
-    key="5"
-    queryName="photo_period"
-    title="Photo Period"
-    absData1="0"
-    absData2="6"
-    absData3="10"
-    absData4="16"
-    absData5="최저권장"
-    absData6="권장"
-    absData7="고권장"
-    absData8="Photo Period 상태에 따른 메시지가 나옵니다."
-  />,
-  <MainLineAreaChart key="6" APIoption="218" ChartName="급수 데이터" />,
-  <MainBarLine2Chart key="7" ChartName="평균 온도" />,
-  <MainLineChart key="8" APIoption="220" ChartName="DLI" />,
-  <MainSliderDiv
-    key="9"
-    queryName="vpd"
-    title="VPD"
-    absData2="1.2"
-    absData3="1.5"
-    absData4="3"
-    absData5="축적"
-    absData6="균형"
-    absData7="증산"
-    absData8="VPD 상태에 따른 메시지가 나옵니다."
-  />,
-  // 완벽한데
-  <MainBarChartLine key="10" ChartName="DLI" />,
-  <MainBarChart key="11" ChartName="Photo Period" />,
-  <MainRTRLineChart key="12" ChartName="RTR" />,
-  // 다음줄
-  <MainSmoothedLineChart
-    key="13"
-    APIoption="198"
-    ChartName="온실 온도"
-    unit="℃"
-  />,
-  <MainSmoothedLineChart
-    key="14"
-    APIoption="199"
-    ChartName="온실 습도"
-    unit="%"
-  />,
-  <MainSmoothedLineChart
-    key="15"
-    APIoption="225"
-    ChartName="온실 CO2"
-    unit="ppm"
-  />,
-  <MainSmoothedLineChartAdd
-    key="16"
-    APIoption="244"
-    ChartName="외부 광량"
-    unit="w/m²"
-  />,
-
-  // 다음줄
-  <MainFootLineChart
-    key="17"
-    APIoption="218"
-    ChartName="물 사용량"
-    unit="liter"
-  />,
+  {
+    id: 0,
+    component: (
+      <TotalReportChart title="온실 환경 종합" time="10:25" size={50} />
+    ),
+    layout: positionMap[0],
+  },
+  { id: 1, component: <GreenhouseScore />, layout: positionMap[1] },
+  { id: 2, component: <CroftGuide />, layout: positionMap[2] },
+  { id: 3, component: <TotalResourceChart />, layout: positionMap[3] },
+  {
+    id: 4,
+    component: (
+      <MainSliderDiv
+        queryName="rtr"
+        title="RTR"
+        absData1="0"
+        absData2="1.2"
+        absData3="1.5"
+        absData4="3"
+        absData5="영양"
+        absData6="균형"
+        absData7="생식"
+        absData8="생식생장, 꽃이 피고 열매가 맺혀요"
+      />
+    ),
+    layout: positionMap[4],
+  },
+  {
+    id: 5,
+    component: (
+      <MainSliderDiv
+        queryName="photo_period"
+        title="Photo Period"
+        absData1="0"
+        absData2="6"
+        absData3="10"
+        absData4="16"
+        absData5="최저권장"
+        absData6="권장"
+        absData7="고권장"
+        absData8="Photo Period 상태에 따른 메시지가 나옵니다."
+      />
+    ),
+    layout: positionMap[5],
+  },
+  {
+    id: 6,
+    component: <MainLineAreaChart APIoption="218" ChartName="급수 데이터" />,
+    layout: positionMap[6],
+  },
+  {
+    id: 7,
+    component: <MainBarLine2Chart ChartName="평균 온도" />,
+    layout: positionMap[7],
+  },
+  {
+    id: 8,
+    component: <MainLineChart APIoption="220" ChartName="DLI" />,
+    layout: positionMap[8],
+  },
+  {
+    id: 9,
+    component: (
+      <MainSliderDiv
+        queryName="vpd"
+        title="VPD"
+        absData2="1.2"
+        absData3="1.5"
+        absData4="3"
+        absData5="축적"
+        absData6="균형"
+        absData7="증산"
+        absData8="VPD 상태에 따른 메시지가 나옵니다."
+      />
+    ),
+    layout: positionMap[9],
+  },
+  {
+    id: 10,
+    component: <MainBarChartLine ChartName="DLI" />,
+    layout: positionMap[10],
+  },
+  {
+    id: 11,
+    component: <MainBarChart ChartName="Photo Period" />,
+    layout: positionMap[11],
+  },
+  {
+    id: 12,
+    component: <MainRTRLineChart ChartName="RTR" />,
+    layout: positionMap[12],
+  },
+  {
+    id: 13,
+    component: (
+      <MainSmoothedLineChart APIoption="198" ChartName="온실 온도" unit="℃" />
+    ),
+    layout: positionMap[13],
+  },
+  {
+    id: 14,
+    component: (
+      <MainSmoothedLineChart APIoption="199" ChartName="온실 습도" unit="%" />
+    ),
+    layout: positionMap[14],
+  },
+  {
+    id: 15,
+    component: (
+      <MainSmoothedLineChart APIoption="225" ChartName="온실 CO2" unit="ppm" />
+    ),
+    layout: positionMap[15],
+  },
+  {
+    id: 16,
+    component: (
+      <MainSmoothedLineChartAdd
+        APIoption="244"
+        ChartName="외부 광량"
+        unit="w/m²"
+      />
+    ),
+    layout: positionMap[16],
+  },
+  {
+    id: 17,
+    component: (
+      <MainFootLineChart APIoption="218" ChartName="물 사용량" unit="liter" />
+    ),
+    layout: positionMap[17],
+  },
 ];
 
 export default GridData;
+
+// import React from "react";
+// import MainSliderDiv from "../../Graphs/MainSliderDiv";
+
+// import MainBarChartLine from "../../Charts/BarCharts/MainBarChartLine";
+// import MainBarChart from "../../Charts/BarCharts/MainBarChart";
+
+// import MainRTRLineChart from "../../Charts/LineCharts/MainRTRLineChart";
+// import MainLineChart from "../../Charts/LineCharts/MainLineChart";
+// import MainFootLineChart from "../../Charts/LineCharts/MainFootLineChart";
+// import MainSmoothedLineChart from "../../Charts/LineCharts/MainSmoothedLineChart";
+// import MainSmoothedLineChartAdd from "../../Charts/LineCharts/MainSmoothedLineChartAdd";
+
+// import MainLineAreaChart from "../../Charts/MixCharts/MainLineAreaChart";
+// import MainBarLine2Chart from "../../Charts/MixCharts/MainBarLine2Chart";
+
+// import TotalReportChart from "../../Charts/TotalReportChart/TotalReportChart";
+// import GreenhouseScore from "../../Charts/GreenhouseScore";
+// import CroftGuide from "../../Charts/CroftGuide/CroftGuide";
+// import TotalResourceChart from "../../Charts/TotalResourceChart/TotalResourceChart";
+
+// const GridData = [
+//   <TotalReportChart key="0" title="온실 환경 종합" time="10:25" size={50} />,
+//   <GreenhouseScore key="1" />,
+//   <CroftGuide key="2" />,
+//   <TotalResourceChart key="3" />, //자원사용량 n월 n주차
+
+//   <MainSliderDiv
+//     key="4"
+//     queryName="rtr"
+//     title="RTR"
+//     absData1="0"
+//     absData2="1.2"
+//     absData3="1.5"
+//     absData4="3"
+//     absData5="영양"
+//     absData6="균형"
+//     absData7="생식"
+//     absData8="생식생장, 꽃이 피고 열매가 맺혀요"
+//   />,
+//   <MainSliderDiv
+//     key="5"
+//     queryName="photo_period"
+//     title="Photo Period"
+//     absData1="0"
+//     absData2="6"
+//     absData3="10"
+//     absData4="16"
+//     absData5="최저권장"
+//     absData6="권장"
+//     absData7="고권장"
+//     absData8="Photo Period 상태에 따른 메시지가 나옵니다."
+//   />,
+//   <MainLineAreaChart key="6" APIoption="218" ChartName="급수 데이터" />,
+//   <MainBarLine2Chart key="7" ChartName="평균 온도" />,
+//   <MainLineChart key="8" APIoption="220" ChartName="DLI" />,
+//   <MainSliderDiv
+//     key="9"
+//     queryName="vpd"
+//     title="VPD"
+//     absData2="1.2"
+//     absData3="1.5"
+//     absData4="3"
+//     absData5="축적"
+//     absData6="균형"
+//     absData7="증산"
+//     absData8="VPD 상태에 따른 메시지가 나옵니다."
+//   />,
+//   // 완벽한데
+//   <MainBarChartLine key="10" ChartName="DLI" />,
+//   <MainBarChart key="11" ChartName="Photo Period" />,
+//   <MainRTRLineChart key="12" ChartName="RTR" />,
+//   // 다음줄
+//   <MainSmoothedLineChart
+//     key="13"
+//     APIoption="198"
+//     ChartName="온실 온도"
+//     unit="℃"
+//   />,
+//   <MainSmoothedLineChart
+//     key="14"
+//     APIoption="199"
+//     ChartName="온실 습도"
+//     unit="%"
+//   />,
+//   <MainSmoothedLineChart
+//     key="15"
+//     APIoption="225"
+//     ChartName="온실 CO2"
+//     unit="ppm"
+//   />,
+//   <MainSmoothedLineChartAdd
+//     key="16"
+//     APIoption="244"
+//     ChartName="외부 광량"
+//     unit="w/m²"
+//   />,
+
+//   // 다음줄
+//   <MainFootLineChart
+//     key="17"
+//     APIoption="218"
+//     ChartName="물 사용량"
+//     unit="liter"
+//   />,
+// ];
+
+// export default GridData;
--- src/component/utils/DND/WonhoGrid.jsx
+++ src/component/utils/DND/WonhoGrid.jsx
@@ -1,96 +1,52 @@
-// WonhoGrid.jsx
 import React, { useState, useEffect } from "react";
 import GridLayout from "react-grid-layout";
 import GridData from "./GridData";
-import TotalResourceChart from "../../Charts/TotalResourceChart/TotalResourceChart";
 
 const WonhoGrid = () => {
-  const filteredInitialData = GridData.filter((_, index) => {
-    return localStorage.getItem(`chartKey_${index}`) !== null;
-  });
-
-  const [wonhoGridData, setWonhoGridData] = useState(filteredInitialData);
+  const [wonhoGridData, setWonhoGridData] = useState([]);
   const [editMode, setEditMode] = useState(false); // 수정 모드 상태
 
-  const calculateLayoutForComponent = (index) => {
-    const positionMap = {
-      0: { x: 0, y: 0 },
-      1: { x: 4, y: 0 },
-      2: { x: 6, y: 0 },
-      3: { x: 0, y: 5 },
-      4: { x: 0, y: 1 },
-      5: { x: 2, y: 1 },
-      6: { x: 4, y: 1 },
-      7: { x: 8, y: 1 },
-      8: { x: 0, y: 2 },
-      9: { x: 2, y: 2 },
-      10: { x: 0, y: 3 },
-      11: { x: 2, y: 3 },
-      12: { x: 4, y: 3 },
-      13: { x: 0, y: 4 },
-      14: { x: 2, y: 4 },
-      15: { x: 4, y: 4 },
-      16: { x: 6, y: 4 },
-      17: { x: 0, y: 6 },
-    };
-
-    const position = positionMap[index];
-    let width;
-    if (index === 6 || index === 7) {
-      width = 3;
-    } else if (index === 0 || index === 2 || index === 3) {
-      width = 4;
+  useEffect(() => {
+    const storedData = localStorage.getItem("wonhoGridComponents");
+    if (storedData) {
+      setWonhoGridData(JSON.parse(storedData));
     } else {
-      width = 2;
+      setWonhoGridData(GridData.filter((item) => [1, 2, 3].includes(item.id)));
     }
-    const height = index === 6 || index === 7 ? 4 : 2;
+  }, []);
 
+  const calculateLayoutForComponent = (item) => {
     return {
-      i: index.toString(),
-      x: position.x,
-      y: position.y,
-      w: width,
-      h: height,
+      i: item.id.toString(), // 여기에서 id를 문자열로 변환
+      x: item.layout.x,
+      y: item.layout.y,
+      w: item.layout.w,
+      h: item.layout.h,
       isResizable: true,
     };
   };
 
-  const initialLayout = GridData.map((_, index) =>
-    calculateLayoutForComponent(index)
+  const [layout, setLayout] = useState(() =>
+    GridData.map((item) => calculateLayoutForComponent(item))
   );
-
-  const addComponent = (newComponent) => {
-    setWonhoGridData([...wonhoGridData, newComponent]);
-    // 필요하면 여기서 layout도 업데이트
-  };
-  const removeComponent = (componentKey) => {
-    const newWonhoGridData = wonhoGridData.filter(
-      (_, index) => index.toString() !== componentKey
-    );
-    setWonhoGridData(newWonhoGridData);
-
-    // 레이아웃을 새롭게 계산합니다. 이 때, 삭제된 컴포넌트의 인덱스를 고려합니다.
-    const newLayout = newWonhoGridData.map((_, index) => {
-      // 여기서 'index'는 새로운 배열의 인덱스이므로, 실제 'positionMap'에서 사용되어야 할
-      // 인덱스와 일치하지 않을 수 있습니다. 이를 매핑하기 위해 원본 배열을 참조합니다.
-      const originalIndex = wonhoGridData.findIndex(
-        (component, compIndex) => compIndex.toString() === index.toString()
-      );
-      return calculateLayoutForComponent(originalIndex);
-    });
-
-    setLayout(newLayout);
-    localStorage.removeItem(`chartKey_${componentKey}`);
-  };
-
-  // 삭제 추가 로직 끝 --------------
-  // 초기 레이아웃 설정
-
-  const [layout, setLayout] = useState(initialLayout);
   const [chartInstances, setChartInstances] = useState({});
 
   const onLayoutChange = (newLayout) => {
-    setLayout(newLayout);
+    if (JSON.stringify(newLayout) !== JSON.stringify(layout)) {
+      // 새로운 레이아웃으로 상태 업데이트
+      setLayout(newLayout);
+
+      // wonhoGridData도 새로운 레이아웃으로 업데이트
+      const updatedWonhoGridData = wonhoGridData.map((item) => {
+        const newItemLayout = newLayout.find(
+          (layoutItem) => layoutItem.i === item.id.toString()
+        );
+        return newItemLayout ? { ...item, layout: newItemLayout } : item;
+      });
+
+      setWonhoGridData(updatedWonhoGridData);
+    }
+
     Object.values(chartInstances).forEach((chart) => chart.resize());
   };
 
@@ -98,20 +54,10 @@ const WonhoGrid = () => {
     setChartInstances((prev) => ({ ...prev, [key]: instance }));
   };
 
-  // 수정모드 토글 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   const toggleEditMode = () => {
     setEditMode(!editMode); // 수정 모드 토글
   };
 
-  useEffect(() => {
-    const filteredData = wonhoGridData.filter((_, index) => {
-      const storedKey = localStorage.getItem(`chartKey_${index}`);
-      console.log(storedKey);
-      return storedKey !== null;
-    });
-
-    setWonhoGridData(filteredData);
-  }, []);
   return (
     <>
       <button onClick={toggleEditMode}>
@@ -120,7 +66,6 @@ const WonhoGrid = () => {
       {/* <TotalResourceChart /> */}
       <GridLayout
         className="layout"
-        layout={layout}
         cols={10}
         rowHeight={100}
         width={1660}
@@ -128,26 +73,19 @@ const WonhoGrid = () => {
         isDraggable={editMode}
         isResizable={editMode} // 전체 레이아웃에 대한 isResizable 설정
       >
-        {wonhoGridData.map((Component, i) => (
+        {wonhoGridData.map((item) => (
           <div
-            key={i.toString()}
+            key={item.id.toString()} // 고유한 key로 item의 id를 사용
             data-grid={{
-              ...layout.find((item) => item.i === i.toString()),
-              isResizable: editMode && !(i === 0 || i === 1 || i === 5), // 개별 아이템에 대한 isResizable 설정
+              ...item.layout, // item의 layout 정보를 사용
+              isResizable:
+                editMode && !(item.id === 0 || item.id === 1 || item.id === 5), // 조건에 따른 isResizable 설정
             }}
           >
-            {editMode && (
-              <button
-                className=""
-                onClick={() => removeComponent(i.toString())}
-              >
-                삭제
-              </button>
-            )}
-            {React.cloneElement(Component, {
-              registerChart,
-              chartKey: i.toString(),
-              layout: layout.find((item) => item.i === i.toString()),
+            {React.cloneElement(item.component, {
+              registerChart: registerChart,
+              chartKey: item.id.toString(),
+              layout: item.layout, // item의 layout 정보 전달
             })}
           </div>
         ))}
