a5e2d0c3412df9b25cc596de2157da33a9cc98ed
Wonho <pabang0620@naver.com>
2024-01-22 21:19:05 +0900

    fasd

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Navbar from './NavBar/NavBar';
-import SideBar from './SideBar/SideBar';
-import { SubNavBarTitle } from './NavBar/SubNavBar/SubNavBarTitle';
-import { useChartData } from '../component/utils/api/Charts/ChartAPI';
+import React, { useState, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "./NavBar/NavBar";
+import SideBar from "./SideBar/SideBar";
+import { SubNavBarTitle } from "./NavBar/SubNavBar/SubNavBarTitle";
+import { useChartData } from "../component/utils/api/Charts/ChartAPI";
 
 const Layout = () => {
   const currentPath = useLocation().pathname;
-  const [container, setContainer] = useState('옥수수 재배 컨테이너');
+  const [container, setContainer] = useState("옥수수 재배 컨테이너");
   const { data, isLoading } = useChartData(
     `${process.env.REACT_APP_BASE_API_KEY}/v1/farms/container-list`,
-    'container-list'
+    "container-list"
   );
   useEffect(() => {
     if (data && !isLoading) {
@@ -26,9 +26,7 @@ const Layout = () => {
           {SubNavBarTitle(currentPath, setContainer, container, setContainer)}
         </>
         <main className="bg-[#E9EBE180]/[0.5] w-full h-full overflow-auto">
-          <Outlet
-            context={{ container, setContainer, currentPath }}
-          />
+          <Outlet context={{ container, setContainer, currentPath }} />
           <div className="flex flex-col absolute bottom-[35px] right-[50px]">
             <img
               className="w-[50px] h-[50px]"
